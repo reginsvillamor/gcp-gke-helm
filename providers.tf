2fcbbfcd00a15d323c5e245ac301ed52b8a51608
reginsvillamor <reginsvillamor@gmail.com>
2022-05-21 16:34:02 +0800

    add kube config

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ data "google_container_cluster" "primary" {
 
 provider "helm" {
   kubernetes {
-    token                  = data.google_client_config.client.access_token
+    token                  = data.google_client_config.provider.access_token
     host                   = data.google_container_cluster.primary.endpoint
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
