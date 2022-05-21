e000c47093005e737ec108c06abfe181926e7c5d
reginsvillamor <reginsvillamor@gmail.com>
2022-05-21 16:33:13 +0800

    add kube config

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -2,6 +2,10 @@ output "name" {
   value = google_container_cluster.primary.name
 }
 
+output "project" {
+  value = google_container_cluster.primary.project
+}
+
 output "location" {
   value = google_container_cluster.primary.location
 }
--- providers.tf
+++ providers.tf
@@ -3,13 +3,14 @@ data "google_client_config" "provider" {}
 
 data "google_container_cluster" "primary" {
   name     = module.gke_cluster.name
+  project  = module.gke_cluster.project
   location = module.gke_cluster.location
 }
 
-provider "kubernetes" {
+provider "helm" {
   kubernetes {
-    host                   = "https://${data.google_container_cluster.primary.endpoint}"
-    token                  = data.google_client_config.provider.access_token
-    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate, )
+    token                  = data.google_client_config.client.access_token
+    host                   = data.google_container_cluster.primary.endpoint
+    cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   }
 }
